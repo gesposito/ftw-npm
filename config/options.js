fa4cab1c75f6e30ab771980782f702a03b76a924
Gianluca Esposito <g@esposi.to>
2015-08-30 00:47:02 +0200

    updated config

--- config/options.js
+++ config/options.js
@@ -3,10 +3,10 @@ var marker = require('./marker.json');
 module.exports = {
   // Fully-qualified URL of a couch database. (Basic auth URLs are ok.)
   'db': 'https://skimdb.npmjs.com/registry',
-  // I'm gonna start from '112532' or '103045', you can use 'now'
-  // marker is going to hold last value even through restarts
+  // You can use a seq number as '112532', '103045' or you can use 'now'
+  // Marker is going to hold last value even through restarts
   // The sequence number to start from. Use "now" to start from the latest change in the DB.
-  'since': marker.seq || '103045',
+  'since': marker.seq || 'now',
   // Milliseconds within which CouchDB must respond (default: 30000 or 30 seconds)
   'heartbeat': 30 * 1000,
   // https://github.com/iriscouch/follow#simple-api-followoptions-callback
