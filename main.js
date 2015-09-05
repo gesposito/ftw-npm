087a4a82fab736488f2369baea4c5b5799e76cb6
Gianluca Esposito <g@esposi.to>
2015-09-05 13:22:19 +0200

    added persistence module

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 var feed = require('./lib/feed.js');
+var persistence = require('./lib/persistence.js');
+
 var options = require('./config/options.js');
 
 // Either retrieve marker from persistence or filesystem
-var base = require('./config/base.json');
-if (base.db) {
-  var Firebase = require("firebase");
-  var ref = new Firebase(base.db);
-  var markerRef = ref.child("marker");
-  
-  markerRef.once("value", function(snapshot) {
+if (persistence.hasDB()) {
+  persistence.getMarker().once("value", function(snapshot) {
     var marker = snapshot.val();
     options.since = marker ? marker.since : 'now';
+
     feed.init(options);
   });
 
